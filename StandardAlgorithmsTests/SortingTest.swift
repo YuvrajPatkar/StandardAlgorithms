fc8386bec05e1c43cda76cad56ae7accba13f7ec
Yuvraj Patkar <patkar.y@etoncollege.org.uk>
2023-11-16 13:02:49 +0000

    adding test target, test plan and sorting test case

---
+++ StandardAlgorithmsTests/SortingTest.swift
@@ -0,0 +1,18 @@
+//
+//  SortingTest.swift
+//  StandardAlgorithmsTests
+//
+//  Created by Patkar, Yuvraj (NA) on 16/11/2023.
+//
+
+import XCTest
+
+final class SortingTest: XCTestCase {
+
+    func testBubbleSortWithShuffled() {
+        //arrange
+        //act
+        //assert
+        XCTAssertTrue(true)
+    }
+}
